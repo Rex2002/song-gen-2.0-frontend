704bb5619a9886bdff680190c38f959df376c0c2
Rex2002 <frahm.benjamin@gmx.de>
2023-03-17 19:50:31 +0100

    added basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Button, Slider } from '@mui/material';
+import React, { useState } from 'react';
 import './App.css';
 
+function valueToBpmText(value: number){
+  return '${number}BPM'
+}
+
+const BpmMarks = [
+  {
+    value: 60,
+    label: '60 BPM',
+  },
+  {
+    value: 180,
+    label: '180 BPM',
+  },
+];
+const GenreMarks = [
+  {
+    value: 0,
+    label: 'Pop',
+  },
+  {
+    value: 1,
+    label: 'Random'
+  },
+  {
+    value: 2,
+    label: 'Blues',
+  },
+];
+
+
 function App() {
+
+  const [BPM, setBPM] = useState(60)
+  const [genre, setGenre] = useState(0)
+
+  const onBpmSliderChange = (e: Event, val: number | number[], activeThumb: number) =>{
+    if(typeof val == "number"){
+      setBPM(val)  
+    }
+  }
+  const onGenreSliderChange = (e: Event, val: number | number[], activeThumb: number) =>{
+    if(typeof val == "number"){
+      setGenre(val)  
+    }
+  }
+  const displayGenre = (genreNo : number) =>{
+    return GenreMarks.filter(el => el.value === genreNo)[0].label
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Song Generator v2
       </header>
+      <body className='App-body'>
+        <div className='App-body-container-left'>
+          <div className='App-body-container-left-left'>          
+            <Slider
+              aria-label="Speed"
+              orientation="vertical"
+              getAriaValueText={valueToBpmText}
+              valueLabelDisplay="off"
+              defaultValue={120}
+              max={180}
+              min={60}
+              marks={BpmMarks}
+              onChange={onBpmSliderChange}
+            />
+          </div>
+          <div className='App-body-container-left-right'>BPM <br/>{BPM}</div>
+        </div>
+        <div className='App-body-container-center'>
+        <Button color='primary' variant='contained' style={{width: "40%"}}>
+          <p className='App-generate-button-text'>
+            Generate Song
+          </p>
+        </Button>
+        </div>
+        <div className='App-body-container-right'>
+        <div className='App-body-container-left-right'> 
+        Genre <br/>{displayGenre(genre)}
+          </div>
+          <div className='App-body-container-left-left'>
+                     
+          <Slider
+              aria-label="Speed"
+              orientation="vertical"
+              getAriaValueText={valueToBpmText}
+              valueLabelDisplay="off"
+              defaultValue={0}
+              step={1}
+              max={2}
+              min={0}
+              marks={GenreMarks}
+              onChange={onGenreSliderChange}
+            /></div>
+        </div>
+
+      </body>
     </div>
   );
 }
